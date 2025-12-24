6f9b4383c99be6b7490b3aee72e88fae43b59d88
SubastralAlok <alokkumaryadav241004@gmail.com>
2025-12-24 17:16:31 +0530

    Updated navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className='navbar'>
-      <div><h1>Horizon Courts</h1></div>
-      <div className="nav-links">
+    <div className="navbar">
+      <div className="logo">
+        <h1>Horizon Courts</h1>
+      </div>
+
+      <div className={`nav-links ${menuOpen ? "active" : ""}`}>
         <a href="#">About Us</a>
         <a href="#">Services</a>
         <a href="#">Coaches</a>
         <a href="#">Events</a>
         <a href="#">Contact</a>
+        <div className="btn mobile-btn">
+          Book Now <i className="ri-arrow-right-up-long-line"></i>
+        </div>
       </div>
-      <div className="btn">Book Now <i className="ri-arrow-right-up-line"></i> </div>
-    </div>
-  )
-}
 
-export default Navbar
+      <div className="btn desktop-btn">
+        Book Now <i className="ri-arrow-right-up-long-line"></i>
+      </div>
+
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <i className={menuOpen ? "ri-close-line" : "ri-menu-3-line"}></i>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
